3d5fb999e650a2b22ee4d266858ec4b7efa4f4f3
sampleanjan <chanivenianjankumarc@gmail.com>
2024-10-17 21:18:19 +0530

    backend setup

---
+++ Backend/ApprovalService/src/main/java/com/example/ApprovalService/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.ApprovalService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApprovalServiceApplication.class);
+	}
+
+}
---
+++ Backend/DashboardService/src/test/java/com/example/DashboardService/DashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DashboardService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
